e52429f64fe62ab74a9abcafc6782fbceede8232
zeph <hi@thatalex.dev>
2022-08-13 06:23:31 -0400

    update to prod sio server

--- static/js/socket.js
+++ static/js/socket.js
@@ -1,4 +1,5 @@
-const socket = io("http://localhost:9800")
+// const socket = io("http://localhost:9800")
+const socket = io("https://api.thatalex.dev");
 
 socket.on("connect", () => {
     console.log("[ws] socket successfully connected");
