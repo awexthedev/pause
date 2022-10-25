3c8828e1b03b8928f820f499552b2b40e108e063
alex 3 <hi@thatalex.dev>
2022-10-25 03:29:17 -0400

    move 100% to realtime/ws api, merge Player/PlayerControls components, fix weird state issuse

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,35 +1,27 @@
 import React from 'react';
 import {checkState} from '../helpers/isdev';
+import {socket} from '../helpers/socket';
 
 let base_url=""
 if (checkState() === "development") base_url="http://localhost:8098/v2/pause/"
 else base_url = "https://api.zephmakes.tech/v2/pause/"
 
 export class Player extends React.Component {
-    render() {
-        return (
-            <div id="container">
-                <div className={"player"}>
-                    <span className={"songinfo"}>
-                        { this.props.d ? <img alt={"bad"} width={"150vw"} src={this.props.d.COVER} /> : <p>bad</p>}
-                        { this.props.d ? <React.Fragment><p>{this.props.d.TITLE}<br />{this.props.d.ARTIST}</p><p>{this.props.d.POSITION} \\ {this.props.d.DURATION}</p></React.Fragment> : <p>Uh oh! Something went wrong.</p>}
-                    </span>
-                </div>
-            </div>
-        )
-    }
-}
+    constructor(props) {
+        super(props);
+        this.state = {
+            d: null,
+            userSetVolume: null
+        }
 
-export class PlayerControls extends React.Component {
-    state = {
-        "shuffle": this.props.d.SHUFFLE,
-        "repeat": this.props.d.REPEAT,
-        "volume": this.props.d.VOLUME,
-        "playState": this.props.d.STATE
+        socket.emit("spotify_status", "listen");
+        socket.on("api_update", async (message) => {
+            if (message) await this.setState({ d: message })
+        })
     }
 
-    handleClicks(i) {
-        if (this.props.d) {
+    async handleClicks(i) {
+        if (this.state.d) {
             console.log(`[Controller] Received '${i}' event, executing`)
 
             fetch(base_url + "player?action=" + i, {
@@ -42,29 +34,30 @@ export class PlayerControls extends React.Component {
                 .catch(function(err) {
                     console.log("[Controller] Something went terribly wrong. Status code " + err.status + " returned from backend.")
                 })
-
-            if (i === "PLAYPAUSE") {
-                if (this.props.d.STATE === 1) this.props.d.STATE = 2
-                else this.props.d.STATE = 1
-            }
         } else console.log(`[Controller] No socket event is stored, aborting click event`)
     }
 
     render() {
         return (
             <div id="container">
-                <div style={{ paddingLeft: '10px' }} id="controls">
+                <div id="player" className={"player"}>
+                    <span className={"songinfo"}>
+                        { this.state.d ? <img alt={"bad"} width={"150vw"} src={this.state.d.COVER} /> : <p>bad</p>}
+                        { this.state.d ? <React.Fragment><p>{this.state.d.TITLE}<br />{this.state.d.ARTIST}</p><p>{this.state.d.POSITION} \\ {this.state.d.DURATION}</p></React.Fragment> : <p>Uh oh! Something went wrong.</p>}
+                    </span>
+                </div>
+                <div style={{ paddingLeft: '30px' }} id="controls">
                     {/* indicators */}
                     <i id="repeat-icon"
                        className="fa fa-repeat"
-                       style={ this.props.d && parseInt(this.props.d.REPEAT) !== 0 ? this.style = { color: 'green', paddingRight: '5px' } : this.style = { color: 'white', paddingRight: '5px' } }
+                       style={ this.state.d && parseInt(this.state.d.REPEAT) !== 0 ? this.style = { color: 'green', paddingRight: '5px' } : this.style = { color: 'white', paddingRight: '5px' } }
                        onClick={() => this.handleClicks("REPEAT")}
                        aria-hidden="true"
                     >
                     </i>
                     <i id="shuffle-icon"
                        className="fa fa-random"
-                       style={ this.props.d && this.props.d.SHUFFLE !== 0 ? this.style = { color: 'green', paddingRight: '5px' } : this.style = { color: 'white', paddingRight: '5px' } }
+                       style={ this.state.d && this.state.d.SHUFFLE !== 0 ? this.style = { color: 'green', paddingRight: '5px' } : this.style = { color: 'white', paddingRight: '5px' } }
                        aria-hidden="true"
                        onClick={() => this.handleClicks("SHUFFLE")}
                     >
@@ -78,7 +71,7 @@ export class PlayerControls extends React.Component {
                     >
                     </i>
                     <i id="play-pause"
-                       className={this.props.d && this.props.d.STATE === 1 ? "fa fa-pause" : "fa fa-play"}
+                       className={this.state.d && this.state.d.STATE === 1 ? "fa fa-pause" : "fa fa-play"}
                        style={{ color: "white", paddingRight: '10px' }}
                        aria-hidden={"true"}
                        onClick={() => {this.handleClicks("PLAYPAUSE")}}
@@ -91,6 +84,32 @@ export class PlayerControls extends React.Component {
                        onClick={() => this.handleClicks("NEXT")}
                     >
                     </i>
+
+                    <input
+                        type="range"
+                        min={0}
+                        max={100}
+                        step={1}
+                        defaultValue={this.state.d ? this.state.d.VOLUME : this.state.userSetVolume}
+                        onChange={event => {
+                            this.setState({ userSetVolume: event.target.valueAsNumber })
+                            socket.emit("spotify_event", {"event":"SETVOLUME","value":event.target.valueAsNumber})
+                        }}
+                    />
+                    <input
+                        type={"text"}
+                        id={"textbox"}
+                        required
+                        placeholder={"your message here.."}
+                        style={{ color:"black", paddingTop:"10px", width:"10vw", height:"3vh"}}
+                    />
+                    <br />
+                    <button
+                        type={"button"}
+                        onClick={() => {
+                            if (document.getElementById("textbox").value) socket.emit("spotify_event", {"event":"NOTIFICATION","value":document.getElementById("textbox").value})
+                        }}
+                    >Submit</button>
                 </div>
             </div>
         )
--- src/index.jsx
+++ src/index.jsx
@@ -3,32 +3,17 @@ import ReactDom from 'react-dom/client';
 
 // components
 import { Header } from './components/Header'
-import { Player, PlayerControls } from './components/Player'
+import { Player } from './components/Player'
 import { Footer } from './components/Footer'
-
-// helpers
-import { socket } from './helpers/socket'
 
 // styling
 import './main.css'
 
 function App() {
-    const [response, setResponse] = React.useState("")
-
-    React.useEffect(() => {
-        socket.on('connect', () => {
-            socket.emit("spotify_status", "listen");
-            socket.on("api_update", async (message) => {
-                if (message) await setResponse(message)
-            })
-        })
-    })
-
     return (
         <div>
             <Header />
-            <Player d={response} />
-            <PlayerControls d={response} active={ response ? "true" : "false" } />
+            <Player />
             <hr width={"50%"} />
             <Footer />
         </div>
