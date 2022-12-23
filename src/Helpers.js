8c69aa78eb3766b0e18c4b9d086484f8fb096de6
alex 3 <hi@thatalex.dev>
2022-12-23 11:26:42 -0500

    Update WS URL

--- src/Helpers.js
+++ src/Helpers.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import io from 'socket.io-client';
 
-export let socket = (checkState() === "development" ? io("http://localhost:9800") : io("https://api.zephmakes.tech"));
+export let socket = (checkState() === "development" ? io("http://localhost:9800") : io("https://ash-api.thatalex.dev"));
 export function checkState() {
     if ('_self' in React.createElement('div')) return "development";
     else return "production";
